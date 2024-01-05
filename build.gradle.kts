134605982405257844b17ea5166cc8bcf46e842c
Roundaround <roundaroundna@gmail.com>
2024-01-04 22:50:01 -0500

    Make anvils use one enchanted book at a time, no matter the actual stack size

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
   id("roundalib") version "0.4.0"
 }
+
+repositories {
+  maven("https://jitpack.io")
+}
+
+dependencies {
+  implementation("io.github.llamalad7:mixinextras-fabric:0.3.2")
+  annotationProcessor("io.github.llamalad7:mixinextras-fabric:0.3.2")
+  include("io.github.llamalad7:mixinextras-fabric:0.3.2")
+}
--- src/main/java/me/roundaround/stackables/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/me/roundaround/stackables/mixin/AnvilScreenHandlerMixin.java
@@ -1,9 +1,12 @@
 package me.roundaround.stackables.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.screen.AnvilScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -31,6 +34,44 @@ public abstract class AnvilScreenHandlerMixin {
     decrementOrRemove(inventory, slot, stack);
   }
 
+  @ModifyExpressionValue(
+      method = "updateResult", at = @At(
+      value = "INVOKE",
+      target = "Lnet/minecraft/inventory/Inventory;getStack(I)Lnet/minecraft/item/ItemStack;",
+      ordinal = 0
+  )
+  )
+  private static ItemStack tweakFirstInputStack(ItemStack stack) {
+    // If either input is an enchanted book, then we can only use one at a time. We do a little
+    // swaperoo to make the updateResult method think there's only one book in the first slot.
+    if (stack.getCount() > 1 && stack.isOf(Items.ENCHANTED_BOOK)) {
+      ItemStack copy = stack.copy();
+      copy.setCount(1);
+      return copy;
+    }
+
+    return stack;
+  }
+
+  @ModifyExpressionValue(
+      method = "updateResult", at = @At(
+      value = "INVOKE",
+      target = "Lnet/minecraft/inventory/Inventory;getStack(I)Lnet/minecraft/item/ItemStack;",
+      ordinal = 1
+  )
+  )
+  private static ItemStack tweakSecondInputStack(ItemStack stack) {
+    // Same as above, but for the second slot.
+    if (stack.getCount() > 1 && stack.isOf(Items.ENCHANTED_BOOK)) {
+      ItemStack copy = stack.copy();
+      copy.setCount(1);
+      return copy;
+    }
+
+    return stack;
+  }
+
+  @Unique
   private static void decrementOrRemove(Inventory inventory, int slot, ItemStack newStack) {
     ItemStack currentStack = inventory.getStack(slot);
 
