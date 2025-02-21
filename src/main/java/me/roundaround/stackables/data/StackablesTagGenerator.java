0bae197f3cd321a0fac6fd7c0dc530cb69263653
Roundaround <roundaroundna@gmail.com>
2025-02-21 14:32:43 -0500

    Format file

--- src/main/java/me/roundaround/stackables/data/StackablesTagGenerator.java
+++ src/main/java/me/roundaround/stackables/data/StackablesTagGenerator.java
@@ -19,44 +19,50 @@ public class StackablesTagGenerator extends FabricTagProvider.ItemTagProvider {
 
   @Override
   protected void configure(RegistryWrapper.WrapperLookup lookup) {
-    getOrCreateTagBuilder(StackablesTags.POTIONS).add(Items.POTION).add(Items.LINGERING_POTION).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.POTIONS).add(Items.POTION).add(Items.LINGERING_POTION).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.SPLASH_POTIONS).add(Items.SPLASH_POTION).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.SPLASH_POTIONS).add(Items.SPLASH_POTION).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.SOUPS).add(Items.MUSHROOM_STEW)
+    this.getOrCreateTagBuilder(StackablesTags.SOUPS)
+        .add(Items.MUSHROOM_STEW)
         .add(Items.RABBIT_STEW)
         .add(Items.BEETROOT_SOUP)
         .add(Items.SUSPICIOUS_STEW)
         .setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.ENCHANTED_BOOKS).add(Items.ENCHANTED_BOOK).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.ENCHANTED_BOOKS).add(Items.ENCHANTED_BOOK).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.BOATS).addOptionalTag(ItemTags.BOATS)
+    this.getOrCreateTagBuilder(StackablesTags.BOATS)
+        .addOptionalTag(ItemTags.BOATS)
         .addOptionalTag(ItemTags.CHEST_BOATS)
         .setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.MINECARTS).add(Items.MINECART)
+    this.getOrCreateTagBuilder(StackablesTags.MINECARTS)
+        .add(Items.MINECART)
         .add(Items.CHEST_MINECART)
         .add(Items.FURNACE_MINECART)
         .add(Items.TNT_MINECART)
         .add(Items.HOPPER_MINECART)
         .setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.BUCKETS).add(Items.POWDER_SNOW_BUCKET)
+    this.getOrCreateTagBuilder(StackablesTags.BUCKETS)
+        .add(Items.POWDER_SNOW_BUCKET)
         .addOptionalTag(ConventionalItemTags.WATER_BUCKETS)
         .addOptionalTag(ConventionalItemTags.LAVA_BUCKETS)
         .setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.EMPTY_BUCKETS).addOptionalTag(ConventionalItemTags.EMPTY_BUCKETS)
+    this.getOrCreateTagBuilder(StackablesTags.EMPTY_BUCKETS)
+        .addOptionalTag(ConventionalItemTags.EMPTY_BUCKETS)
         .setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.BEDS).addOptionalTag(ItemTags.BEDS).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.BEDS).addOptionalTag(ItemTags.BEDS).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.MUSIC_DISCS).addOptionalTag(ItemTags.MUSIC_DISCS).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.MUSIC_DISCS).addOptionalTag(ItemTags.MUSIC_DISCS).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.INSTRUMENTS).add(Items.GOAT_HORN).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.INSTRUMENTS).add(Items.GOAT_HORN).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.BANNER_PATTERNS).add(Items.FLOWER_BANNER_PATTERN)
+    this.getOrCreateTagBuilder(StackablesTags.BANNER_PATTERNS)
+        .add(Items.FLOWER_BANNER_PATTERN)
         .add(Items.CREEPER_BANNER_PATTERN)
         .add(Items.SKULL_BANNER_PATTERN)
         .add(Items.MOJANG_BANNER_PATTERN)
@@ -64,33 +70,36 @@ public class StackablesTagGenerator extends FabricTagProvider.ItemTagProvider {
         .add(Items.PIGLIN_BANNER_PATTERN)
         .setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.HORSE_EQUIPMENT).add(Items.SADDLE)
+    this.getOrCreateTagBuilder(StackablesTags.HORSE_EQUIPMENT)
+        .add(Items.SADDLE)
         .add(Items.LEATHER_HORSE_ARMOR)
         .add(Items.IRON_HORSE_ARMOR)
         .add(Items.GOLDEN_HORSE_ARMOR)
         .add(Items.DIAMOND_HORSE_ARMOR)
         .setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.CAKES).add(Items.CAKE).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.CAKES).add(Items.CAKE).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.TOTEMS).add(Items.TOTEM_OF_UNDYING).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.TOTEMS).add(Items.TOTEM_OF_UNDYING).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.ARMOR_STANDS).add(Items.ARMOR_STAND).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.ARMOR_STANDS).add(Items.ARMOR_STAND).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.BANNERS).addOptionalTag(ItemTags.BANNERS).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.BANNERS).addOptionalTag(ItemTags.BANNERS).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.SIGNS).addOptionalTag(ItemTags.SIGNS)
+    this.getOrCreateTagBuilder(StackablesTags.SIGNS)
+        .addOptionalTag(ItemTags.SIGNS)
         .addOptionalTag(ItemTags.HANGING_SIGNS)
         .setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.THROWABLES).add(Items.SNOWBALL).add(Items.EGG).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.THROWABLES).add(Items.SNOWBALL).add(Items.EGG).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.ENDER_PEARLS).add(Items.ENDER_PEARL).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.ENDER_PEARLS).add(Items.ENDER_PEARL).setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.DRINKABLES).add(Items.HONEY_BOTTLE)
+    this.getOrCreateTagBuilder(StackablesTags.DRINKABLES)
+        .add(Items.HONEY_BOTTLE)
         .addOptionalTag(ConventionalItemTags.MILK_BUCKETS)
         .setReplace(true);
 
-    getOrCreateTagBuilder(StackablesTags.SIGNED_BOOKS).add(Items.WRITTEN_BOOK).setReplace(true);
+    this.getOrCreateTagBuilder(StackablesTags.SIGNED_BOOKS).add(Items.WRITTEN_BOOK).setReplace(true);
   }
 }
